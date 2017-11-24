07956a1c299c186f02a879a99d9579e9fd753066
Selenite <selenevallet29@gmail.com>
2017-11-24 16:54:46 +0100

    Ajout de nouveaux points gps (ikea, carrefour, intermarche, liberte) et changement couleur de point

--- app/src/main/java/com/rezigo/rezigo/ActivityMaps.java
+++ app/src/main/java/com/rezigo/rezigo/ActivityMaps.java
@@ -1,5 +1,6 @@
 package com.rezigo.rezigo;
 
+import android.graphics.Bitmap;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -7,6 +8,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -38,13 +41,19 @@ public class ActivityMaps extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng rezid = new LatLng(48.363416, -4.566971);
-        mMap.addMarker(new MarkerOptions().position(rezid).title("Rezid"));
+        //Points GPS
+        LatLng Rezid = new LatLng(48.363416, -4.566971);
+        mMap.addMarker(new MarkerOptions().position(Rezid).title("Rezid").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        LatLng CarrefourPlouz = new LatLng(48.373172, -4.587265);
+        mMap.addMarker(new MarkerOptions().position(CarrefourPlouz).title("Carrefour Plouzané"));
+        LatLng IKEA  = new LatLng(48.417857, -4.441834);
+        mMap.addMarker(new MarkerOptions().position(IKEA).title("IKEA"));
+        LatLng liberte  = new LatLng(48.390781, -4.485713);
+        mMap.addMarker(new MarkerOptions().position(liberte).title("Place de la liberté"));
+        LatLng intermarcherPlouz  = new LatLng(48.379001, -4.601233);
+        mMap.addMarker(new MarkerOptions().position(intermarcherPlouz).title("Intermarché Super Plouzané"));
+        //Zoom
         float zoomLevel = 12.0f;
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(rezid, zoomLevel));
-
-        LatLng carefourPlouz = new LatLng(48.373172, -4.587265);
-        mMap.addMarker(new MarkerOptions().position(carefourPlouz).title("Carrefour Plouzane"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Rezid, zoomLevel));
     }
 }

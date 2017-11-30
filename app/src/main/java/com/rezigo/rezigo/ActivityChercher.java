fd8977479d1cc46644d6f2e95ac05cc3df02dc0b
Selenite <selenevallet29@gmail.com>
2017-11-30 13:28:00 +0100

    Ajout activity List relié a rechercher un trajet et ajout navbar a l'activity Chercher

--- app/src/main/java/com/rezigo/rezigo/ActivityChercher.java
+++ app/src/main/java/com/rezigo/rezigo/ActivityChercher.java
@@ -1,13 +1,54 @@
 package com.rezigo.rezigo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class ActivityChercher extends AppCompatActivity {
+public class ActivityChercher extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chercher);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button_rechercher :
+                startActivityList();
+                break;
+            case R.id.button_info :
+                startActivityInformation();
+                break;
+            case R.id.button_maps:
+                startActivityMaps();
+                break;
+            case R.id.button_leave:
+                startActivityLeave();
+                break;
+            case R.id.button_home :
+                finish();
+                break;
+        }
+    }
+    private void startActivityMaps() {
+        Intent intent = new Intent(ActivityChercher.this, ActivityMaps.class);
+        startActivity(intent);
+    }
+
+    private void startActivityLeave() {
+        Intent intent = new Intent(ActivityChercher.this, ActivityLeave.class);
+        startActivity(intent);
+    }
+    private void startActivityInformation() {
+        Intent intent = new  Intent(ActivityChercher.this, ActivityInformation.class);
+        startActivity(intent);
+    }
+    private void startActivityList() {
+        Intent intent = new  Intent(ActivityChercher.this, ActivityList.class);
+        startActivity(intent);
     }
 }

d99917b810e389776441dae6fbed17d35afbfdaf
Selenite <selenevallet29@gmail.com>
2017-11-30 13:52:04 +0100

    ajout des navbar sur les activités ActivityList & ActivityProposer

--- app/src/main/java/com/rezigo/rezigo/ActivityList.java
+++ app/src/main/java/com/rezigo/rezigo/ActivityList.java
@@ -1,7 +1,9 @@
 package com.rezigo.rezigo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityList extends AppCompatActivity {
 
@@ -10,4 +12,36 @@ public class ActivityList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
     }
+
+
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button_info :
+                startActivityInformation();
+                break;
+            case R.id.button_maps:
+                startActivityMaps();
+                break;
+            case R.id.button_leave:
+                startActivityLeave();
+                break;
+            case R.id.button_home :
+                finish();
+                break;
+        }
+    }
+    private void startActivityMaps() {
+        Intent intent = new Intent(ActivityList.this, ActivityMaps.class);
+        startActivity(intent);
+    }
+
+    private void startActivityLeave() {
+        Intent intent = new Intent(ActivityList.this, ActivityLeave.class);
+        startActivity(intent);
+    }
+    private void startActivityInformation() {
+        Intent intent = new  Intent(ActivityList.this, ActivityInformation.class);
+        startActivity(intent);
+    }
+
 }
